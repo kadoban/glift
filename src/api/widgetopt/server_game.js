40f1644bb91a0af15a9927d388013054945843ce
Joshua Simmons <joshua.simmons@emptypath.com>
2016-05-10 14:34:14 -0700

    add a 'SERVER_GAME' widget type and associated controller
    Just a copy of GAME_VIEWER to start with, will be editing it.

---
+++ src/api/widgetopt/server_game.js
@@ -0,0 +1,48 @@
+/**
+ * Additional options for a go server's board
+ */
+glift.api.widgetopt[glift.enums.widgetTypes.SERVER_GAME] = {
+  markLastMove: true,
+  enableMousewheel: true,
+
+  keyMappings: {
+    ARROW_LEFT: 'iconActions.arrowleft.click',
+    ARROW_RIGHT: 'iconActions.arrowright.click',
+    ',': 'iconActions.arrowleft.click',
+    '.': 'iconActions.arrowright.click',
+    '<': 'iconActions.jump-left-arrow.click',
+    '>': 'iconActions.jump-right-arrow.click',
+    /** Toggle the selected variation. */
+    ']': function(widget) {
+      widget.controller.moveUpVariations();
+      widget.applyBoardData(widget.controller.flattenedState());
+    },
+    /** Toggle the selected variation. */
+    '[': function(widget) {
+      widget.controller.moveDownVariations();
+      widget.applyBoardData(widget.controller.flattenedState());
+    }
+  },
+
+  problemConditions: {}, // Disable problem evaluations
+
+  controllerFunc: glift.controllers.serverGame,
+
+  icons: ['jump-left-arrow', 'jump-right-arrow', 'arrowleft', 'arrowright'],
+
+  showVariations: glift.enums.showVariations.MORE_THAN_ONE,
+
+  statusBarIcons: [
+    'game-info',
+    'move-indicator',
+    'fullscreen'
+  ],
+
+  stoneClick: function(event, widget, pt) {
+    var currentPlayer = widget.controller.getCurrentPlayer();
+    var partialData = widget.controller.addStone(pt, currentPlayer);
+    widget.applyBoardData(partialData);
+  },
+  stoneMouseover: undefined, // rely on defaults
+  stoneMouseout: undefined, // rely on defaults
+};
---
+++ src/controllers/server_game.js
@@ -0,0 +1,135 @@
+/**
+ * A ServerGame encapsulates the idea of viewing/editing a game
+ * on a go server.
+ *
+ * @type {!glift.controllers.ControllerFunc}
+ */
+glift.controllers.serverGame = function(sgfOptions) {
+  var ctrl = glift.controllers;
+  var baseController = glift.util.beget(ctrl.base());
+  var newController = glift.util.setMethods(baseController,
+      ctrl.ServerGameMethods);
+  newController.initOptions(sgfOptions);
+  return newController;
+};
+
+glift.controllers.ServerGameMethods = {
+  /**
+   * Called during initOptions, in the BaseController.
+   *
+   * This creates a treepath (a persisted treepath) and an index into the
+   * treepath.  This allows us to 'remember' the last variation taken by the
+   * player, which seems to be the standard behavior.
+   */
+  extraOptions: function() {},
+
+  /**
+   * Find the variation associated with the played move.
+   *
+   * Returns null if the addStone operation isn't possible.
+   */
+  addStone: function(point, color) {
+    var possibleMap = this._possibleNextMoves();
+    var key = point.toString() + '-' + color;
+    if (possibleMap[key] === undefined) {
+      return null;
+    }
+    var nextVariationNum = possibleMap[key];
+    return this.nextMove(nextVariationNum);
+  },
+
+  /**
+   * Go back to the previous branch or comment.
+   *
+   * If maxMovesPrevious is defined, then we cap the number of moves at
+   * maxMovesPrevious. Otherwise, we keep going until we hit the beginning of
+   * the game.
+   *
+   * Returns null in the case that we're at the root already.
+   */
+  previousCommentOrBranch: function(maxMovesPrevious) {
+    var displayDataList = []; // TODO(kashomon): Merge this together?
+    var displayData = null;
+    var movesSeen = 0;
+    var comment;
+    var numChildren;
+    do {
+      displayData = this.prevMove();
+      comment = this.movetree.properties().getOneValue('C');
+      numChildren = this.movetree.node().numChildren();
+      movesSeen++;
+      if (maxMovesPrevious && movesSeen === maxMovesPrevious) {
+        break;
+      }
+    } while (displayData && !comment && numChildren <= 1);
+    // It's more expected to reset the 'next' variation to zero.
+    this.setNextVariation(0);
+    return this.flattenedState();
+  },
+
+  /**
+   * Go to the next branch or comment.
+   *
+   * If maxMovesNext is defined, then we cap the number of moves at
+   * maxMovesNext. Otherwise, we keep going until we hit the beginning of
+   * the game.
+   *
+   * Returns null in the case that we're at the root already.
+   */
+  nextCommentOrBranch: function(maxMovesNext) {
+    var displayData = null;
+    var movesSeen = 0;
+    var comment;
+    var numChildren;
+    do {
+      displayData = this.nextMove();
+      comment = this.movetree.properties().getOneValue('C');
+      numChildren = this.movetree.node().numChildren();
+      movesSeen++;
+      if (maxMovesNext && movesSeen === maxMovesNext) {
+        break;
+      }
+    } while (displayData && !comment && numChildren <= 1); 
+    return this.flattenedState();
+  },
+
+  /**
+   * Move up what variation will be next retrieved.
+   */
+  moveUpVariations: function() {
+    return this.setNextVariation((this.nextVariationNumber() + 1) %
+        this.movetree.node().numChildren());
+  },
+
+  /**
+   * Move down  what variation will be next retrieved.
+   */
+  moveDownVariations: function() {
+    // Module is defined incorrectly for negative numbers.  So, we need to add n
+    // to the result.
+    return this.setNextVariation((this.nextVariationNumber() - 1 +
+        this.movetree.node().numChildren()) %
+        this.movetree.node().numChildren());
+  },
+
+  /**
+   * Get the possible next moves.  Used to verify that a click is actually
+   * reasonable.
+   *
+   * Implemented as a map from point-string+color to variationNumber:
+   *  e.g., pt-BLACK : 1.  For pass, we use 'PASS' as the point string.  This is
+   *  sort of a hack and should maybe be rethought.
+   */
+  _possibleNextMoves: function() {
+    var possibleMap = {};
+    var nextMoves = this.movetree.nextMoves();
+    for (var i = 0; i < nextMoves.length; i++) {
+      var move = nextMoves[i];
+      var firstString = move.point !== undefined ?
+          move.point.toString() : 'PASS';
+      var key = firstString + '-' + (move.color);
+      possibleMap[key] = i;
+    }
+    return possibleMap;
+  }
+};
